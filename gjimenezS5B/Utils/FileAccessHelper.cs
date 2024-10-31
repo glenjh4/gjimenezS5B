0a547011cb17dc89df43243a30880421d104f832
master <master@SMLAB306PC12_25>
2024-10-30 20:17:20 -0500

    cambios

---
+++ gjimenezS5B/Utils/FileAccessHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gjimenezS5B.Utils
+{
+    internal class FileAccessHelper
+    {
+    }
+}
