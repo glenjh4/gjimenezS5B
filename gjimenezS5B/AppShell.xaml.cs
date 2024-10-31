dd22fd71abb56041979808c611da16cb8a14f496
master <master@SMLAB306PC12_25>
2024-10-30 20:09:28 -0500

    Agregar archivos de proyecto.

---
+++ gjimenezS5B/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace gjimenezS5B
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ gjimenezS5B/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace gjimenezS5B
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
